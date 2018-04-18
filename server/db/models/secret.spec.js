e619c29d19747c4c59bf747ef2a9e4daf7e392c5
Kevin Kelly <kvinklly@gmail.com>
2018-04-18 16:48:46 -0500

    secrets spec created

---
+++ server/db/models/secret.spec.js
@@ -0,0 +1,51 @@
+/* global describe beforeEach it */
+
+const { expect } = require('chai');
+const db = require('../index');
+const Secret = db.model('secret');
+const User = db.model('user');
+const { seedSecrets, seedUsers } = require('./test-seeds');
+const assert = require('assert');
+
+describe('Secret model', () => {
+  before(async () => {
+    await db.sync({ force: true });
+    const users = await User.bulkCreate(seedUsers);
+    const secrets = await Secret.bulkCreate(seedSecrets);
+    assert(users.length === 3, 'User.bulkCreate failed');
+    assert(secrets.length === 3, 'Secret.bulkCreate failed');
+  });
+
+  it('should be true', () => {
+    expect(true).to.equal(true);
+  });
+
+  // beforeEach(() => {
+  //   return Passage.create({
+  //     title: 'Hello World',
+  //     content: 'Hello yee world of mine!'
+  //   })
+  //   .then(passage => {
+  //     hello = passage;
+  //   })
+  //   .catch(err => {
+  //     console.log(err.message);
+  //   });
+  // });
+
+  // describe('Model assignments', () => {
+  //   it('Defaults to not public', () => {
+  //     expect(hello.isPublic).to.be.equal(false);
+  //   });
+  // });
+
+
+  // describe('Methods', () => {
+
+  //   it('Preview returns a slice the length you provide it', () => {
+  //     expect(hello.preview(3).length).to.be.equal(3);
+  //     expect(hello.preview(3)).to.be.equal('Hel');
+  //   });
+  // });//end instance methods
+}); // end describe('Passage model')
+
---
+++ server/db/models/test-seeds.js
@@ -0,0 +1,40 @@
+const seedUsers = [
+  {
+    id: 1,
+    email: 'user1@email.com',
+    password: '123',
+    isAdmin: false
+  }, {
+    id: 2,
+    email: 'user2@email.com',
+    password: '123',
+    isAdmin: false
+  }, {
+    id: 3,
+    email: 'admin@email.com',
+    password: '321321',
+    isAdmin: true
+  }
+];
+
+const seedSecrets = [
+  {
+    message: 'I love the admin of this web site.',
+    userId: 1,
+    isPublic: true
+  }, {
+    message: 'Once I shoplifted from Macys.',
+    userId: 1,
+    isPublic: false
+  }, {
+    message: 'I took the ACT for my sister. I got a terrible score.',
+    userId: 2,
+    isPublic: true
+  }, {
+    message: 'I kissed a girl and I liked it.',
+    userId: 2,
+    isPublic: false
+  }
+];
+
+module.exports = { seedUsers, seedSecrets };
