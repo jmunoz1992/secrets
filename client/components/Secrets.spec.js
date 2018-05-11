97598ff8445bb08cf99dde0cb3b0240a43325296
Kevin Kelly <kvinklly@gmail.com>
2018-05-11 14:41:17 -0500

    fixed tests

--- client/components/Secrets.spec.js
+++ client/components/Secrets.spec.js
@@ -6,6 +6,10 @@ import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 import { seedSecrets } from '../../script/test-seeds';
 
+const seeds = seedSecrets.map((seedSecret, index) => {
+  return { ...seedSecret, id: index + 1 };
+});
+
 const adapter = new Adapter();
 enzyme.configure({adapter});
 
@@ -13,7 +17,7 @@ describe('<Secrets />', () => {
   let secretsWrapper;
 
   beforeEach(() => {
-    secretsWrapper = shallow(<Secrets secrets={seedSecrets} />);
+    secretsWrapper = shallow(<Secrets secrets={seeds} />);
   });
 
   it('should render secrets within a container', () => {
@@ -28,12 +32,14 @@ describe('<Secrets />', () => {
     const secretChildren = secretsWrapper.find(Secret);
     expect(secretChildren.at(0).props().secret)
       .to.deep.equal({
+        id: 1,
         message: 'I love the admin of this web site.',
         userId: 1,
         isPublic: true
       });
     expect(secretChildren.at(1).props().secret)
       .to.deep.equal({
+        id: 3,
         message: 'I took the ACT for my sister. I got a terrible score.',
         userId: 2,
         isPublic: true
