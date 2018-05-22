2007fa521955056b5de2a1c61d29d9156b13a52a
Kevin Kelly <kvinklly@gmail.com>
2018-05-22 15:34:44 -0500

    remove solution, uncomment code, reset secrets.js

--- server/api/secrets.js
+++ server/api/secrets.js
@@ -1,73 +1,26 @@
 const router = require('express').Router();
 const { Secret } = require('../db/models');
 
-const isLoggedIn = (req, res, next) => {
-  if (!req.user) {
-    res.sendStatus(401);
-  } else {
-    next();
-  }
-};
-
-const belongsTo = (req, res, next) => {
-  const userId = req.user.id;
-  Secret.findById(+req.params.id)
-    .then(secret => {
-      if (secret.userId !== userId) {
-        res.sendStatus(401);
-      } else {
-        next();
-      }
-    });
-};
-
 router.get('/', (req, res, next) => {
-  const userId = req.user && req.user.id ? req.user.id : null;
   Secret.findAll()
-  .then(secrets => {
-    secrets = secrets.filter(secret => {
-      return secret.isPublic || secret.userId === userId;
-    });
-    secrets = secrets.map(secret => ({
-      id: secret.id,
-      message: secret.message,
-      userId: secret.userId === userId ? userId : null,
-      isPublic: secret.isPublic
-    }));
-    res.json(secrets);
-  })
+  .then(secrets => res.status(200).json(secrets))
   .catch(next);
 });
 
-router.post('/', isLoggedIn, (req, res, next) => {
-  const secret = {
-    message: req.body.message,
-    userId: req.user.id
-  };
-
-  Secret.create(secret)
+router.post('/', (req, res, next) => {
+  Secret.create(req.body)
     .then(newSecret => res.status(201).json(newSecret))
     .catch(next);
 });
 
-router.put('/:id', isLoggedIn, belongsTo, (req, res, next) => {
-  const secret = { isPublic: req.body.isPublic };
-  Secret.update(
-    secret,
-    {
-      where: {
-        id: req.params.id
-      },
-      returning: true,
-      plain: true
-    }
-  )
-    .then(result => result[1].dataValues)
-    .then(updatedSecret => res.status(200).json(updatedSecret))
+router.put('/:id', (req, res, next) => {
+  Secret.findById(req.params.id)
+    .then(secret => secret.update(req.body))
+    .then(secret => res.status(200).json(secret))
     .catch(next);
 });
 
-router.delete('/:id', isLoggedIn, belongsTo, (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   Secret.destroy({
     where: {
       id: req.params.id
