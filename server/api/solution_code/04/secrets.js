0b473952eac4657068c6bb20849cc1fa429f2e57
Kevin Kelly <kvinklly@gmail.com>
2018-05-25 14:25:19 -0500

    solution 04 done

---
+++ server/api/solution_code/04/secrets.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const { Secret } = require('../db/models');
+const { Op } = require('sequelize');
+
+router.get('/', (req, res, next) => {
+  let where = { isPublic: true };
+  if (req.user) {
+    where = { [Op.or]: [{userId: req.user.id}, { isPublic: true }] };
+  }
+
+  const userId = req.user && req.user.id ? req.user.id : null;
+  Secret.findAll({ where })
+    .then(result => {
+      const secrets = result.map(secret => {
+        return {
+          id: secret.id,
+          message: secret.message,
+          isPublic: secret.isPublic,
+          userId: secret.userId === userId ? userId : null
+        };
+      });
+      res.status(200).json(secrets);
+    })
+    .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+  if (!req.user) {
+    res.sendStatus(401);
+  } else {
+    Secret.create({
+      message: req.body.message,
+      isPublic: req.body.isPublic,
+      userId: req.user.id
+    })
+      .then(newSecret => res.status(201).json(newSecret))
+      .catch(next);
+  }
+});
+
+router.put('/:id', (req, res, next) => {
+  if (!req.user) {
+    res.sendStatus(401);
+  } else {
+    Secret.findById(req.params.id)
+      .then(secret => {
+        if (secret.userId !== req.user.id) {
+          res.sendStatus(401);
+          return;
+        }
+        return secret.update({ isPublic: req.body.isPublic });
+      })
+      .then(secret => res.status(200).json(secret))
+      .catch(next);
+  }
+});
+
+router.delete('/:id', (req, res, next) => {
+  if (!req.user) {
+    res.sendStatus(401);
+  } else {
+    Secret.findById(req.params.id)
+      .then(secret => {
+        if (secret.userId !== req.user.id) {
+          res.sendStatus(401);
+        } else {
+          return res.sendStatus(200);
+        }
+      })
+      .catch(next);
+  }
+});
+
+module.exports = router;
